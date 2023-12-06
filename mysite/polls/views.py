555447802f32d3ccddc74d97fa6b93f3d4356afe
Bombwhale <solkblte@icloud.com>
2023-12-06 14:14:50 +0900

    part 4

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,23 +1,44 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def results(request, question_id):
-    response = f"You're looking at the results of question {question_id}."
-    return HttpResponse(response.encode())
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.".encode())
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "항목을 선택하지 않았습니다.",
+            },
+        )
+
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    # POST를 잘 처리하고 난 뒤에는 항상 HttpResponseRedirect를 반환해야 함.
+    # reverse는 URL 하드코딩하지 않도록 도와줌 
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))

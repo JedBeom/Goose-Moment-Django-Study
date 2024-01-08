60d53be8ada0df417c1613d6478bb9c8207ce2f4
Bombwhale <solkblte@icloud.com>
2024-01-08 21:43:39 +0900

    MDN React(EN) 1. Getting started with React

---
+++ moz-todo-react-vite/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App(props) {
+	return (
+		<>
+			<header>
+				<h1>Hello, World!</h1>
+				<button type="button" className="primary"> 
+					Click me!
+				</button>
+			</header>
+		</>
+	);
+}
+
+export default App;
+
